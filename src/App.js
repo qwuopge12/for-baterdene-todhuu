9c6752a5afa3289ab9b99f1f45d44b29cd29a67b
qwuopge12 <amarbaysgalanbayarchimeg@gmail.com>
2023-02-02 21:40:36 +0800

    amra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './app1.css'
+import Zurag from './assets/hq720.webp'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [newItem,setNewItem] = useState('')
+  const [item,setItem] = useState([])
+  const [file,setFile] = useState('')
+
+  const fileUpload = (niga) => {
+        setFile(URL.createObjectURL(niga.target.files[0]))
+  }
+  const addItem = () =>{
+    const itemuud = {
+      id: Math.floor(Math.random() * 99999999999999),
+      img:file,
+      value: newItem,
+    }
+    
+    setItem(oldlist => [...oldlist, itemuud])
+
+  }
+  const amaRaaDelete = (id) =>{    
+    const arr = item.filter(amaraa => amaraa.id !== id )
+    setItem(arr)
+  }
+return (
+    <div className='dripp'>
+      <div className='input-button'>
+        <input type="text" value={newItem} onChange={e => setNewItem(e.target.value)} placeholder="   Type your title..."/>
+        <input className='bolog' type="file"  onChange={fileUpload} />
+        <button onClick = {()=>addItem()}>Create</button>
+      </div>
+        <div className='drippy'>
+            {item.map((items) =>{
+                return(
+                  <div className='style-from'>                                          
+                      <img className='ghj' src={items.img} alt=''/>
+                      <div className='skip-title'>                      
+                        <div className='pext'>{items.value}</div>   
+                        <button onClick={() => amaRaaDelete(items.id)}></button>  
+                    </div>             
+                  </div>
+                )
+            })}
+        </div>      
     </div>
-  );
+);
 }
 
 export default App;
